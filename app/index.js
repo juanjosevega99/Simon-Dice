c513f9ce0cd7bacc50a84f91626c0418990828cf
Juan Jose Vega <juanvega.tech@gmail.com>
2020-12-28 22:00:30 -0500

    finished index config

--- app/index.js
+++ app/index.js
@@ -1,7 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./util/database');
 
-const app = express()
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/dev', require('./routes/dev'));
+app.use(
+  '/users',
+  require('./routes/users')
+)(async () => {
+  try {
+    await sequelize.sync({ force: false });
+    app.listen(process.env.EXTERNAL_PORT);
+  } catch (error) {
+    console.log(error);
+  }
+});
